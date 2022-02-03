9d9c43dbb1ff8b0c3316ec01608212f05beedf0a
dkakinoki <d.kakinoki@gmail.com>
2022-02-03 23:12:12 +0900

    dev

--- .github/workflows/test.rb
+++ .github/workflows/test.rb
@@ -1,3 +1,4 @@
 require 'net/http'
 require 'uri'
-  print Net::HTTP.get(URI.parse('https://jsonplaceholder.typicode.com/todos/1'))
+print Net::HTTP.get(URI.parse('https://jsonplaceholder.typicode.com/todos/1'))
+print ENV["TRELLO_TOKEN"]
